05009dc89ae56d9eb5b2a17d9b6984648b2f8611
aximof <60539757+aximu@users.noreply.github.com>
2021-08-19 11:19:35 +0800

    LED

---
+++ 驱动篇/LED-并行/flash_module.v
@@ -0,0 +1,37 @@
+ /* LED 闪烁 */
+ 
+ module flash_module(
+     CLK,
+     RSTn,
+     LED_Out
+ );
+
+    input CLK;
+    input RSTn;
+    output LED_Out;
+
+    parameter T50MS = 22'd2_499_999;
+
+    reg [21:0] Count1;
+
+    always @(posedge CLK or negedge RSTn) begin
+        if(!RSTn)
+            Count1 <= 22'd0;
+        else if(Count1 == T50MS)
+            Count1 <= 22'd0;
+        else 
+            Count1 <= Count1 + 1'b1;
+    end
+
+    reg rLED_Out;
+
+    always @(posedge CLK or negedge RSTn) begin
+        if(!RSTn)
+            rLED_Out <= 1'b0;
+        else if(Count1 == T50MS)
+            rLED_Out <= ~rLED_Out;
+    end
+
+    assign LED_Out = rLED_Out;
+
+endmodule
